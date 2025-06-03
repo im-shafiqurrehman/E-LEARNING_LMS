1a6f4fbb89a6d78b0c88e8111a60065a2d2eec60
ShafiqUr-Rehman <shafiqurrehmanbscs2022@gmail.com>
2025-06-03 20:46:43 +0500

    serving frontend by backend

--- server/app.js
+++ server/app.js
@@ -1,35 +1,47 @@
-// app.ts
 import express from "express";
 export const app = express();
+
 import cookieParser from "cookie-parser";
 import Cors from "cors";
-require("dotenv").config();
-import { Request, Response, NextFunction } from "express";
-import { ErrorMiddleWare } from "./utils/middleware/error";
-import userRouter from "./routes/user.route";
-import courseRouter from "./routes/course.route";
-import orderRouter from "./routes/order.route";
-import notificationRouter from "./routes/notification.route";
-import layoutRouter from "./routes/layout.route";
-import analyticsRouter from "./routes/analytics.route";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { ErrorMiddleWare } from "./utils/middleware/error.js";
+
+import userRouter from "./routes/user.route.js";
+import courseRouter from "./routes/course.route.js";
+import orderRouter from "./routes/order.route.js";
+import notificationRouter from "./routes/notification.route.js";
+import layoutRouter from "./routes/layout.route.js";
+import analyticsRouter from "./routes/analytics.route.js";
+
 import { rateLimit } from "express-rate-limit";
+
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Use this for __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
+
 app.use(
   Cors({
     origin: [
       "http://localhost:3000",
-      "https://e-learning-lms-frontend-theta.vercel.app",      
+      "https://e-learning-lms-frontend-theta.vercel.app",
     ],
     credentials: true,
   })
 );
 
+const clientBuildPath = path.join(__dirname, "../client/.next");
+
 // ✅ API rate limiting (before routes)
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes in mili-sec
+  windowMs: 15 * 60 * 1000, // 15 minutes in milli-sec
   max: 100,
   standardHeaders: "draft-7",
   legacyHeaders: false,
@@ -47,16 +59,15 @@ app.use("/api/v1", notificationRouter);
 app.use("/api/v1", analyticsRouter);
 app.use("/api/v1", layoutRouter);
 
-app.get("/test", (req: Request, res: Response) => {
+app.get("/test", (req, res) => {
   res.status(200).json({
     success: true,
     message: "API is working",
   });
 });
 
-
-app.all("*", (req: Request, res: Response, next: NextFunction) => {
-  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+app.all("*", (req, res, next) => {
+  const err = new Error(`Route ${req.originalUrl} not found`);
   err.statusCode = 404;
   next(err);
 });
@@ -64,6 +75,5 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
 // ✅ Global Error Handler Middleware
 app.use(ErrorMiddleWare);
 
-
-// Export for Vercel (Important)
+// Export for Vercel or server.js
 export default app;
